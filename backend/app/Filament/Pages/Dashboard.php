e94940f87aa30959143b8aaa54763580f13e5bdc
Matheus Edivaldo <ms1501085@gmail.com>
2026-08-24 22:19:34 -0300

    fix: update Dashboard column configuration and add WelcomeWidget to AdminPanelProvider

--- backend/app/Filament/Pages/Dashboard.php
+++ backend/app/Filament/Pages/Dashboard.php
@@ -10,7 +10,7 @@ class Dashboard extends BaseDashboard
     {
         return [
             'default' => 1,
-            'md' => 3,
+            'md' => 2,
         ];
     }
 }
--- backend/app/Providers/Filament/AdminPanelProvider.php
+++ backend/app/Providers/Filament/AdminPanelProvider.php
@@ -6,6 +6,7 @@ use App\Filament\Pages\Dashboard;
 use App\Filament\Widgets\GalleryStatsOverview;
 use App\Filament\Widgets\MenuStatsOverview;
 use App\Filament\Widgets\WebmailAccessWidget;
+use App\Filament\Widgets\WelcomeWidget;
 use Filament\Http\Middleware\Authenticate;
 use Filament\Http\Middleware\AuthenticateSession;
 use Filament\Http\Middleware\DisableBladeIconComponents;
@@ -14,7 +15,6 @@ use Filament\Panel;
 use Filament\PanelProvider;
 use Filament\Support\Assets\Css;
 use Filament\Support\Colors\Color;
-use Filament\Widgets\AccountWidget;
 use Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse;
 use Illuminate\Cookie\Middleware\EncryptCookies;
 use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken;
@@ -50,7 +50,7 @@ class AdminPanelProvider extends PanelProvider
             ])
             ->discoverWidgets(in: app_path('Filament/Widgets'), for: 'App\Filament\Widgets')
             ->widgets([
-                AccountWidget::class,
+                WelcomeWidget::class,
                 WebmailAccessWidget::class,
                 GalleryStatsOverview::class,
                 MenuStatsOverview::class,
